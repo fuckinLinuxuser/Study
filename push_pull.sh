52ac9644be2686a9ae05583334cf664d90186c94
fuckinLinuxuser <samotuga621@gmail.com>
2025-10-30 14:08:57 +0300

    2025-10-30 14:08

--- push_pull.sh
+++ push_pull.sh
@@ -1,14 +1,13 @@
 #!/bin/zsh
 
-if true; then
+while true; do
     commit_message=$(date +"%Y-%m-%d %H:%M")
     git add .
     git commit -m "$commit_message"
     git pull
     git push
     git pull
-fi
+done
 
-else
-    git merge
-fi
+exit 0
+
